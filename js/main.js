928ff7e61dd80e2053367057994deaac162887ba
Juliana Hong <julianahhong@gmail.com>
2016-11-23 00:08:52 -0500

    Add back and next buttons

--- js/main.js
+++ js/main.js
@@ -2,6 +2,27 @@ var i=0;
 var images = ["img/apples.jpg", "img/caramel.jpg", "img/cider.jpg", "img/people.jpg", "img/wine.jpg"];
 var image = document.getElementById("photo");
 
+function nextImage(){
+	if (i == images.length - 1) {
+		i = 0;
+	}
+	else{
+		i+=1;
+	}
+	image.src = images[i];
+	highlightDot();
+}
+function previousImage(){
+	if (i == 0){
+		i = images.length - 1;
+	}
+	else{
+		i-=1;
+	}
+	image.src = images[i];
+}
+
+/*
 function changeSliderImages(){
 	//for (var i = 0; i<images.length; i++) {
 		var image = document.getElementById("photo");
@@ -13,22 +34,4 @@ function changeSliderImages(){
 
 //change image automatically every 2 sec
 setTimeout(changeSliderImages(), 2000);
-
-function nextImage(){
-	if (i == images.length - 1) {
-		i = 0;
-	}
-	else{
-		i+=1;
-	}
-	image.src = images[i];
-}
-function previousImage(){
-	if (i == 0){
-		i = images.length - 1;
-	}
-	else{
-		i-=1;
-	}
-	image.src = images[i];
-}
+*/
