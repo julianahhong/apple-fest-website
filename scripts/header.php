52bbb940aa3351e10a689d4aec98d266f3045d45
HilarieSit <hs764@cornell.edu>
2016-11-23 20:03:35 -0500

    Create header.php

---
+++ scripts/header.php
@@ -0,0 +1,17 @@
+<?php
+echo '
+<body>
+<div class="banner">
+  <img src="images/cornell-university-landscape.jpg" alt="cornell">
+  <!-- image credit: http://kai.kendal.org/wp-content/blogs.dir/11/files/home-slideshow/cornell-university-landscape.jpg -->
+</div>
+
+<div id="nav_bar">
+  <ul>
+    <li><a href="index.php">Homepage</a></li>
+    <li><a href="about.php">History</a></li>
+    <li><a href="about.php">Event&Food</a></li>
+    <li><a href="about.php">Directions</a></li>
+  </ul>
+</div> <!--end of nav_bar div-->';
+?>
